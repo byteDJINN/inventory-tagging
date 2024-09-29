64a9c0ca31db5daca17c896c4864d2af423bb13d
Djimon <developerdjay@gmail.com>
2024-09-29 13:01:26 +0800

    .

--- cloud/src/routes/authentication/[slug]/+page.js
+++ cloud/src/routes/authentication/[slug]/+page.js
@@ -5,24 +5,6 @@ export async function load({ params }) {
 	// const { title, dir } = post.metadata;
 	const content = post.default;
 
-	let r1;
-
-	fetch('http://localhost:9876/api/button-count')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        r1 = data;
-        console.log("@@@",r1); // Output the result
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
-
-
 	return {
 		content,
 		params:params.slug
